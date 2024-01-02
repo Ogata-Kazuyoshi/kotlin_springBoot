ea8cfa5aba65dee4844d0d611d10ccdc1e9cf893
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-01-03 00:32:05 +0900

    initial commit

---
+++ src/main/kotlin/com/example/sample3/Controller.kt
@@ -0,0 +1,14 @@
+package com.example.sample3
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HelloController {
+    @GetMapping("/")
+    fun index(model:Model):String {
+        model.addAttribute("message","Hello World!!!Kotlin!!!!")
+        return "index"
+    }
+}
---
+++ src/main/kotlin/com/example/sample3/Sample3Application.kt
@@ -0,0 +1,11 @@
+package com.example.sample3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Sample3Application
+
+fun main(args: Array<String>) {
+	runApplication<Sample3Application>(*args)
+}
